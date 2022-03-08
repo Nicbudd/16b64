1fcbe8e1f25773d6b27f9a87bb8ccc763587ef08
Nicbudd <niczippy77@gmail.com>
2022-03-08 12:39:11 -0500

    removed python asciitranslator

--- asciitranslator.py
+++
@@ -1,8 +0,0 @@
-constants = [""] * 2**16
-
-with open("16b64constants.txt") as fp:
-    for line in fp:
-        cList = line[9:].strip().split(", ")
-        constants[int(line[2:6], 16)] = cList[0]
-
-print(constants[:1000])
